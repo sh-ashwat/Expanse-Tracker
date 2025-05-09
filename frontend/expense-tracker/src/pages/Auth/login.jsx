63a1f54fff50155d960f83f1be825aee06afc9d1
Shashwat Singh <singhsha345@gmail.com>
2025-05-09 12:35:05 +0530

    fixed-filename-issue

--- frontend/expense-tracker/src/pages/Auth/login.jsx
+++ frontend/expense-tracker/src/pages/Auth/login.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from 'react';
-import AuthLayout from '../../components/layouts/Authlayout';
+import AuthLayout from '../../components/layouts/AuthLayout';
 import { useNavigate } from 'react-router-dom';
 import Input from '../../components/Inputs/Input';
 import { Link } from 'react-router-dom';
